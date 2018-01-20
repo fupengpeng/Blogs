6c3cb925f94c32736a9e497d0ecf48d03283acb1
fupengpeng <564055710@qq.com>
2018-01-20 14:06:36 +0800

    框架搭建

---
+++ src/com/jdlx/blogs/domain/User.java
@@ -0,0 +1,15 @@
+package com.jdlx.blogs.domain;
+
+/**
+ * 
+ * @Title: User
+ * @Description: 用户
+ * @Company: 山东九点连线信息技术有限公司
+ * @ProjectName: Blogs
+ * @author fupengpeng
+ * @date 2018年1月20日 下午1:41:16
+ */
+public class User {
+	private Long id;
+	
+}
